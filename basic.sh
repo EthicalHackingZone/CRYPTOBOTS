6ec548f2a9167eba57f0a5c37594e0411046f1e3
Ethical-Hacking-Zone <55155544+Ethical-Hacking-Zone@users.noreply.github.com>
2020-02-26 22:48:43 +0530

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,47 @@
+echo ""
+echo ""
+echo "             EHZ TUITORIALS "
+echo ""
+echo "
+╔═╗┬ ┬┌┐ ┌─┐┬─┐  ┬ ┬┌─┐┬ ┬┬┌─
+║  └┬┘├┴┐├┤ ├┬┘  ├─┤├─┤│││├┴┐
+╚═╝ ┴ └─┘└─┘┴└─  ┴ ┴┴ ┴└┴┘┴ ┴
+"
+echo ""
+
+echo "SIT BACK AND RELAX"
+echo ""
+echo ""
+
+
+echo "THIS WILL TAKE CARE OF EVERYTHING"
+echo ""
+echo ""
+
+echo "YOU WILL BE TOLD ONCE WHEN DONE"
+echo ""
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pip install bs4
+pip install rsa
+pip install pyaes
+pip install requests
+pip install asyncio
+pip install async_generator
+pip install colorama
+unzip ltc01.zip
+unzip btc1.zip
+unzip doge1.zip
+unzip zec1.zip
+unzip bch1.zip
+rm -rf ltc01.zip
+rm -rf btc1.zip
+rm -rf doge1.zip
+rm -rf zec1.zip
+rm -rf bch1.zip
+clear
+rm -rf basic.sh
+echo " DONE "
+echo "exiting"
+echo "exit"
